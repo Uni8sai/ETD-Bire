a60488958fd59ef7513bc1952b676b021249af2b
Uni8sai <kasukaosu@gmail.com>
2025-01-31 12:30:55 +0900

    add interface debug

--- LinkThread.py
+++ LinkThread.py
@@ -46,6 +46,7 @@ class LinkThread(QThread):
     def connect_encrypt(self, interface, bss, wifi_password):
         iface_list = PyWiFi().interfaces()
         print("Detected interfaces:", [iface.name() for iface in iface_list])
+        print("Scan list:", [(ap.ssid, ap.bssid, ap.akm) for ap in scan_list])
         self.notify_Label.emit('Disconnecting from the current network...')
         interface.disconnect()
         time.sleep(2)
