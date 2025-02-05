b4949b07a53a4d1259be123bdc8b3a2aaaac6959
Uni8sai <kasukaosu@gmail.com>
2025-02-05 16:53:08 +0900

    add interface debug

--- MonitorThread.py
+++ MonitorThread.py
@@ -3,7 +3,7 @@ from scapy.all import *
 from pywifi import PyWiFi, const
 
 class MonitorThread(QThread):
-    notify_Progress = pyqtSignal(str)
+    notify_Progress = pyqtSignal(str,str)
     def __init__(self, face_name, parent=None):
         super(MonitorThread, self).__init__(parent)
         self.iface_name = face_name
@@ -15,8 +15,9 @@ class MonitorThread(QThread):
         while self.running:
             status = interface.status()
             status_text = self.get_status_text(status)
-            print(f"Interface: {interface.name()} Status: {status_text}")
-            self.notify_Progress.emit(f"Interface: {interface.name()} Status: {status_text}")
+            interface_text = interface.name()
+            print(f"Interface: {interface_text} Status: {status_text}")
+            self.notify_Progress.emit(f"Interface: {interface_text} Status: {status_text}")
             time.sleep(2)  # 2秒ごとに状態を確認
 
     def stop(self):
